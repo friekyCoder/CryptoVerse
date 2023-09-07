3353e5545894c9f9ddc223dcfe987399c7272269
harshit <harshit302001n@gmail.com>
2023-09-07 13:49:06 +0530

    first commit

---
+++ src/CryptoContext.js
@@ -0,0 +1,27 @@
+import React, { useContext, useState } from "react";
+import { useEffect } from "react";
+import { createContext } from "react";
+
+const Crypto = createContext();
+
+const CryptoContext = ({ children }) => {
+  const [currency, setCurrency] = useState("INR");
+  const [symbol, setsymbol] = useState("₹");
+
+  useEffect(() => {
+    if (currency === "INR") setsymbol("₹");
+    else if (currency === "USD") setsymbol("$");
+  }, [currency]);
+
+  return (
+    <Crypto.Provider value={{ currency, symbol, setCurrency }}>
+      {children}
+    </Crypto.Provider>
+  );
+};
+
+export default CryptoContext;
+
+export const CryptoState = () => {
+  return useContext(Crypto);
+};
